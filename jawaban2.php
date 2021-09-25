a0fbbf8f03d38dab374bf2ffe60bbcd7c2ed8259
qorthony <ahmad.qorthony@gmail.com>
2021-09-25 12:29:09 +0700

    first commit

---
+++ jawaban2.php
@@ -0,0 +1,8 @@
+<?php
+
+$alas = 12;
+$tinggi = 6;
+
+$luasSegitiga = 1/2*$alas*$tinggi;
+
+echo $luasSegitiga;
---
+++ jawaban3.php
@@ -0,0 +1,9 @@
+<?php
+
+define("PI", 3.14);
+
+$r = 14;
+
+$luasLingkaran = PI*$r**2;
+
+echo $luasLingkaran;
